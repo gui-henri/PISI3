01ca89c9c56337f3f7ed993179b5f6746d18764b
Guilherme Henrique <guihenrique.dev@gmail.com>
2022-12-13 20:09:57 -0300

    Estrutura de arquivos agora permite múltiplas páginas

---
+++ data/streamlit_project/Introdução.py
@@ -0,0 +1,40 @@
+import streamlit as st
+
+# crescimento do lucro dos filmes no tempo
+# analisar os outliners
+# corrigir pela inflação
+# correlação entre faturam
+# ento e orçamento e tentar achar um valor ótimo/ mais eficiente
+# tentar prever o crescimento da industria
+# crescimento do orçamento no tempo
+# crescimento do faturamento no tempo
+
+st.title("Painel de Administração do MoviePicker")
+st.write("O Painel de Administração do MoviePicker é uma ferramenta criada para auxiliar na análise dos dados do sistema e fornecer insights que podem ser usados para tomada de decisão.")
+st.markdown(
+    """
+    **👈 Selecione uma guia** para ver cada ferramenta ou análise.
+    #### Páginas
+    - Introdução: Nossa página principal. Apertar nela o trará de volta para cá!
+    - Pré-processamento: Aqui estão documentadas as etapas necessárias para tratar o dataset.
+    - Dataset: Descrição detalhada das mais diversas informações a respeito do dataset e suas variáveis.
+    - Visualizar filme: Busque as principais informações a respeito de um filme.
+    - Comparador de filmes: Compare diretamente as principais métricas de vários filmes.
+    - Margem de lucro: Observe a margem de lucro de um filme. Também contém diferentes modelos
+    - Mais em breve :D
+
+    #### Mais informações?
+
+    - Visite nosso [Github](https://github.com/gui-henri/PISI3)
+    - Visualize o [Dataset](https://www.kaggle.com/datasets/tmdb/tmdb-movie-metadata?select=tmdb_5000_movies.csv)
+
+    """
+)
+st.sidebar.success("Selecione acima a página que gostaria de visitar")
+
+
+# Avatar, Indiana Jones
+
+# Orçamento, Faturamento, Popularidade, Tempo de duração, Número de votos, Nota
+#df.set_index('original_title', inplace=True)
+
--- data/streamlit_project/pages/4_Comparador_de_filmes.py
+++ data/streamlit_project/pages/4_Comparador_de_filmes.py
@@ -1,74 +1,12 @@
-import streamlit as st
 import pandas as pd
-import ast
-
+import streamlit as st
 import streamlit as st
 import pandas as pd
 import matplotlib.pylab as plt
 import seaborn as sns
 
-# crescimento do lucro dos filmes no tempo
-# analisar os outliners
-# corrigir pela inflação
-# correlação entre faturam
-# ento e orçamento e tentar achar um valor ótimo/ mais eficiente
-# tentar prever o crescimento da industria
-# crescimento do orçamento no tempo
-# crescimento do faturamento no tempo
-
-st.title("TMDB Dataset")
-st.write("A tabela a seguir é uma pequena visualização, mostrando o formato dos dados extraidos.")
-
 df = pd.read_csv('../archive/TMDB_5000_movies.csv')
-
-st.write(df.head())
-
-st.title("Visualização de filmes")
-filme = st.selectbox(
-    "Selecione um filme: ",
-    df['original_title']
-)
-if filme:
-    df.set_index('original_title', inplace=True)
-    filme_info = df.loc[filme]
-
-    st.header(filme_info['title'])
-    st.subheader(filme_info['tagline'])
-    st.write(filme_info['overview'])
-    generos_list = [x["name"] for x in ast.literal_eval(filme_info["genres"])]
-    generos_formated_to_print = [f"{x}, " for x in generos_list]
-    generos_formated_to_print[-1] = generos_formated_to_print[-1].replace(", ", "")
-    st.header("Gêneros:")
-    st.write(*generos_formated_to_print)
-
-    c1, c2, c3 = st.columns(3)
-    with c1:
-        st.subheader("Orçamento")
-        st.markdown(f"{filme_info['budget']:,}")
-    with c2:
-        st.subheader("Faturamento")
-        st.markdown(f"{filme_info['revenue']:,}")
-    with c3:
-        st.subheader("Data de lançamento")
-        st.write(filme_info['release_date'])
-    
-    c4, c5, c6, c7 = st.columns(4)
-
-    with c4:
-        st.write(f"Tempo de duração: {int(filme_info['runtime'])} minutos")
-    with c5:
-        st.write(f"Popularidade: {filme_info['popularity']}")
-    with c6:
-        st.write(f"Nota no IMDB: {filme_info['vote_average']}")
-    with c7:
-        st.write(f"Número de votos: {filme_info['vote_count']}")
-else:
-    st.write("Por favor, selecione um filme")
-
-# Avatar, Indiana Jones
-
-# Orçamento, Faturamento, Popularidade, Tempo de duração, Número de votos, Nota
-#df.set_index('original_title', inplace=True)
+df.set_index('original_title', inplace=True)
 
 st.title("Comparar filmes")
 
@@ -120,8 +58,8 @@ def orc_x_fat(films):
     st.pyplot(fig)
 
 
-def execute_comparason(comparason):
-    comparason(selected_movies)
+def execute_comparason(comparason, movies):
+    comparason(movies)
 
 lista = {
     "Orçamento x Faturamento": orc_x_fat,
@@ -134,4 +72,4 @@ lista = {
 selected_movies = st.multiselect("Selecione filmes para comparar: ", df['title'])
 chosen_comparason = st.selectbox("Selecione o parâmetro", ["Orçamento x Faturamento", "Popularidade", "Tempo de duração", "Quantidade de votos", "Nota IMDB"])
 
-execute_comparason(lista[chosen_comparason])
+execute_comparason(lista[chosen_comparason], selected_movies)
