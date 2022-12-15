5d6a4a0f0bf0e0e50c71cd623ed7f71d29efc7d5
Lucas <62450459+lucaslombardi07@users.noreply.github.com>
2022-12-14 21:02:39 -0300

    Atualização dos gráficos

--- data/streamlit_project/pages/4_Comparador_de_filmes.py
+++ data/streamlit_project/pages/4_Comparador_de_filmes.py
@@ -1,37 +1,43 @@
 import pandas as pd
 import streamlit as st
-import matplotlib.pylab as plt
-import seaborn as sns
+import plotly.express as px
 
 df = pd.read_csv('../archive/TMDB_5000_movies.csv')
 df.set_index('original_title', inplace=True)
 
 st.title("Comparar filmes")
 
-def orc_x_fat(films):
-    partial_data = [df.loc[film] for film in films]
-    data_films = [x[["budget", "revenue"]] for x in partial_data]
-    st.bar_chart(data_films)
-
 def popularity(films):
+    if len(films) == 0:
+        return "Nada para ver aqui"
+
     partial_data = [df.loc[film] for film in films]
     data_films = [x[["popularity"]] for x in partial_data]
-    st.bar_chart(data_films)
+    st.plotly_chart(px.bar(data_films, x="popularity", labels={"popularity":"Nota", "index": "Titulo"}))
 
 def duration(films):
+    if len(films) == 0:
+        return "Nada para ver aqui"
+
     partial_data = [df.loc[film] for film in films]
     data_films = [x[["runtime"]] for x in partial_data]
-    st.bar_chart(data_films)
+    st.plotly_chart(px.bar(data_films, x="runtime", labels={"runtime":"Duração minutos", "index": "Titulo"}))
 
 def votes(films):
+    if len(films) == 0:
+        return "Nada para ver aqui"
+
     partial_data = [df.loc[film] for film in films]
     data_films = [x[["vote_count"]] for x in partial_data]
-    st.bar_chart(data_films)
+    st.plotly_chart(px.bar(data_films, x="vote_count", labels={"vote_count":"Votos", "index": "Titulo"}))
 
 def note(films):
+    if len(films) == 0:
+        return "Nada para ver aqui"
+
     partial_data = [df.loc[film] for film in films]
     data_films = [x[["vote_average"]] for x in partial_data]
-    st.bar_chart(data_films)
+    st.plotly_chart(px.bar(data_films, x="vote_average", labels={"vote_average":"Nota", "index": "Titulo"}))
     
 def orc_x_fat(films):
 
@@ -41,20 +47,7 @@ def orc_x_fat(films):
     partial_data = [df.loc[film] for film in films]
     lista = [[f["title"], f["budget"], f["revenue"]] for f in partial_data]
     to_stream = pd.DataFrame(lista, columns=("Títulos", "Orçamento", "Faturamento"))    
-    
-    plt.rcParams.update({"font.size": 14, "font.weight": "bold"})
-    fig, ax = plt.subplots(figsize=(12, 8),)
-    sns.set_style("whitegrid")
-    to_stream.plot.barh(x="Títulos", figsize=(12, 8), width=0.9, ax=ax)
-    plt.title("Orçamento x Faturamento", fontsize=24, fontweight="bold")
-    plt.legend(loc="lower right")
-    for patch in ax.patches:
-        w, h = patch.get_width(), patch.get_height()
-        y = patch.get_y()
-        ax.text(w + -0.1, h / 2 + y, f"{w:.3f}", va="center")
-
-    st.pyplot(fig)
-
+    st.plotly_chart(px.bar(data_frame=to_stream, x=["Orçamento", "Faturamento"], y="Títulos", barmode='group', labels={"value":"US$"}))
 
 def execute_comparason(comparason, movies):
     comparason(movies)
--- data/streamlit_project/pages/5_Visão_Global.py
+++ data/streamlit_project/pages/5_Visão_Global.py
@@ -1,24 +1,13 @@
 import pandas as pd
 import streamlit as st
-import seaborn as sns
+import plotly.express as plt
 
 df = pd.read_csv('../archive/TMDB_5000_movies.csv')
 df.set_index('original_title', inplace=True)
 
 st.title("Visão global")
 
-sns.set_theme(style="whitegrid")
-cmap = sns.cubehelix_palette(rot=-.2, as_cmap=True)
-g = sns.relplot(
-    data=df,
-    x="budget", y="revenue", hue= "revenue"-"budget", legend="auto", 
-    palette=cmap,
-)
+g = plt.scatter(data_frame= df, x="budget", y= "revenue", hover_name= "title", labels={"revenue":"Arrecadado", "budget":"Faturamento"})
 
-g.set(xscale="linear", yscale="linear")
-g.ax.xaxis.grid(True, "minor", linewidth=.25)
-g.ax.yaxis.grid(True, "minor", linewidth=.25)
-g.despine(left=True, bottom=True)
-st.pyplot(g)
-
-st.subheader("Plot de todos os filmes no banco de dados, em realação ao seu orçamento (eixo horizontal) e lucro (eixo vertical).")
+st.plotly_chart(g)
+st.subheader("Plot de todos os filmes no banco de dados, em realação ao seu orçamento (eixo horizontal) e Faturamento (eixo vertical).")
