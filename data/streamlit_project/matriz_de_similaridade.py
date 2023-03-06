867fed5b0cd2493f7617784f96fc7462ba3c3ac4
FernandaSouzaa <104859098+FernandaSouzaa@users.noreply.github.com>
2023-03-06 14:28:09 -0300

    Add matriz de similaridade

---
+++ data/streamlit_project/matriz_de_similaridade.py
@@ -0,0 +1,27 @@
+import pandas as pd
+from comparar import comp
+  
+df = pd.read_csv('C:/Users/Lombardi/Documents/GitHub/PISI3/data/streamlit_project/archive/tmdb_3000_movies.csv')
+filmes = df.values.tolist()
+    
+tags = ['budget', 'genres', 'id', 'keywords', 'original_language', 'original_title', 'overview',
+        'popularity', 'production_companies', 'production_countries', 'release_date', 'revenue',
+        'runtime', 'title', 'vote_average', 'vote_count']
+
+pesos = {'budget': 3, 'genres': 8, 'id': 0, 'keywords': 2, 'original_language': 1, 'original_title': 0, 'overview': 0,
+        'popularity': 3, 'production_companies': 1, 'production_countries': 1, 'release_date': 2, 'revenue': 4,
+        'runtime': 3, 'title': 0, 'vote_average': 7, 'vote_count': 6}
+
+matriz = []
+temp = []
+
+t = len(filmes)
+
+for i, filmeA in enumerate(filmes):
+    print(f"Processando... {i}/{t}", end = '\r')
+    for filmeB in filmes[i+1:]:
+        temp.append(comp(filmeA, filmeB, tags, pesos))
+    matriz.append(temp)
+    temp = []
+
+print(matriz)
