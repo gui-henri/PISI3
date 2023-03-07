5429e6b7139f4ab69c257877cbde0f8307633491
RennanJSM <87147821+RennanJSM@users.noreply.github.com>
2023-03-07 12:16:10 -0300

    Update icon added

--- movie_picker/lib/pages/main_page/favorites_tab.dart
+++ movie_picker/lib/pages/main_page/favorites_tab.dart
@@ -11,25 +11,36 @@ class FavoritesTab extends StatelessWidget {
             .toList();
 
     return Scaffold(
-      body: Ink(
-          decoration: mpDefaultBackgroundDecoration(),
-          child: GridView.builder(
-              gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
-                  maxCrossAxisExtent: 160,
-                  childAspectRatio: 5 / 7,
-                  crossAxisSpacing: 15,
-                  mainAxisSpacing: 2),
-              itemCount: myProducts.length,
-              itemBuilder: (BuildContext ctx, index) {
-                return Container(
-                  margin: const EdgeInsets.all(7),
-                  alignment: Alignment.center,
-                  decoration: BoxDecoration(
-                      color: const Color.fromARGB(255, 234, 226, 203),
-                      borderRadius: BorderRadius.circular(10)),
-                  child: Text(myProducts[index]["name"]),
-                );
-              })),
-    );
+        body: Ink(
+            decoration: mpDefaultBackgroundDecoration(),
+            child: GridView.builder(
+                gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
+                    maxCrossAxisExtent: 160,
+                    childAspectRatio: 5 / 7,
+                    crossAxisSpacing: 15,
+                    mainAxisSpacing: 2),
+                itemCount: myProducts.length,
+                itemBuilder: (BuildContext ctx, index) {
+                  return Container(
+                    margin: const EdgeInsets.all(7),
+                    alignment: Alignment.bottomRight,
+                    decoration: BoxDecoration(
+                        color: const Color.fromARGB(255, 255, 255, 255),
+                        borderRadius: BorderRadius.circular(10)),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text(myProducts[index]["name"]),
+                        IconButton(
+                          onPressed: () {},
+                          icon: const Icon(Icons.favorite, color: Colors.red),
+                          padding: EdgeInsets.zero,
+                          constraints: const BoxConstraints(),
+                          splashRadius: 15,
+                        )
+                      ],
+                    ),
+                  );
+                })));
   }
 }
