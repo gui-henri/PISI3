d27e906bd1d8c012a9720ccde5c8242dc8884c90
Lucas Lombardi <lucaslombardi07@gmail.com>
2023-03-07 16:14:59 -0300

    Correções e atualizações na geração da matriz de similaridade

--- data/streamlit_project/matrix_generation/comparar.py
+++ data/streamlit_project/matrix_generation/comparar.py
@@ -1,58 +1,58 @@
-def budget (k, filmeA, filmeB):
-    return funDiff(filmeA[k], filmeB[k])
+def budget (filmeA, filmeB):
+    return funDiff(filmeA, filmeB)
 
-def genres (k, filmeA, filmeB):
-    return funMatches(k, filmeA, filmeB)
+def genres (filmeA, filmeB):
+    return funMatches(filmeA, filmeB)
             
-def id (k, filmeA, filmeB):
+def id (filmeA, filmeB):
     return 0
     
-def keywords (k, filmeA, filmeB):
-    return funMatches(k, filmeA, filmeB)
+def keywords (filmeA, filmeB):
+    return funMatches(filmeA, filmeB)
 
-def original_language (k, filmeA, filmeB):
+def original_language (filmeA, filmeB):
     return int(filmeA == filmeB)
     
-def original_title (k, filmeA, filmeB):
+def original_title (filmeA, filmeB):
     return 0
 
-def overview (k, filmeA, filmeB):
+def overview (filmeA, filmeB):
     return 0
 
-def popularity (k, filmeA, filmeB):
-    return funDiff(filmeA[k], filmeB[k])
+def popularity (filmeA, filmeB):
+    return funDiff(filmeA, filmeB)
 
-def production_companies (k, filmeA, filmeB):
-    return funMatches(k, filmeA, filmeB)
+def production_companies (filmeA, filmeB):
+    return funMatches(filmeA, filmeB)
 
-def production_countries (k, filmeA, filmeB):
-    return funMatches(k, filmeA, filmeB)
+def production_countries (filmeA, filmeB):
+    return funMatches(filmeA, filmeB)
 
-def release_date (k, filmeA, filmeB):
+def release_date (filmeA, filmeB):
     scale = 4
-    return funDiff(int(filmeA[k][:4]), int(filmeB[k][:4]), scale)
+    return funDiff(int(filmeA[:4]), int(filmeB[:4]), scale)
 
-def revenue (k, filmeA, filmeB): 
-    return funDiff(filmeA[k], filmeB[k])
+def revenue (filmeA, filmeB): 
+    return funDiff(filmeA, filmeB)
 
-def runtime (k, filmeA, filmeB):
-    return funDiff(filmeA[k], filmeB[k])
+def runtime (filmeA, filmeB):
+    return funDiff(filmeA, filmeB)
 
-def title (k, filmeA, filmeB):
+def title (filmeA, filmeB):
     return 0
 
-def vote_average (k, filmeA, filmeB):
-    return funDiff(filmeA[k], filmeB[k])
+def vote_average (filmeA, filmeB):
+    return funDiff(filmeA, filmeB)
 
-def vote_count (k, filmeA, filmeB):
-    return funDiff(filmeA[k], filmeB[k])
+def vote_count (filmeA, filmeB):
+    return funDiff(filmeA, filmeB)
 
-def funMatches (k, filmeA, filmeB):
+def funMatches (filmeA, filmeB):
     x = 0
-    for genero in filmeA[k]:
-        if genero in filmeB[k]:
-            x += 1            
-    return x/max(len(filmeA[k]), len(filmeB[k]))
+    for item in filmeA:
+        if item in filmeB:
+            x += 1      
+    return x/max(len(filmeA), len(filmeB))
 
 def funDiff(valA, valB, scale = 0):
     if scale == 0:
@@ -64,5 +64,5 @@ def comp(filmeA, filmeB, tags, peso):
     x = 0
     for k, tag in enumerate(tags):
         f = eval(tag)
-        x += f(k, filmeA, filmeB) * peso[tag]
+        x += f(filmeA[k], filmeB[k]) * peso[tag]
     return x
--- data/streamlit_project/matrix_generation/matriz_de_similaridade.py
+++ data/streamlit_project/matrix_generation/matriz_de_similaridade.py
@@ -1,32 +1,38 @@
 import pandas as pd
-from .comparar import comp
+from ast import literal_eval
+from comparar import comp
 
-def generate_matrix(filmes, tags, pesos):
-    matriz = []
+tags = ['budget', 'genres', 'id', 'keywords', 'original_language', 'original_title', 'overview',
+        'popularity', 'production_companies', 'production_countries', 'release_date', 'revenue',
+        'runtime', 'title', 'vote_average', 'vote_count']
+
+pesos = {'budget': 1, 'genres': 5, 'id': 0, 
+         'keywords': 7, 'original_language': 1, 
+         'original_title': 0, 'overview': 0,
+        'popularity': 2, 'production_companies': 1, 
+        'production_countries': 1, 'release_date': 3, 
+        'revenue': 2, 'runtime': 1, 'title': 0, 
+        'vote_average': 2, 'vote_count': 1}
+    
+df = pd.read_csv('../data/archive/tmdb_3000_movies.csv', converters={'genres': literal_eval, 'keywords': literal_eval, 'production_companies': literal_eval, 'production_countries': literal_eval,})
+
+filmes = df.values.tolist()
+
+matriz = []
+
+temp = []
+
+t = len(filmes)
+for i, filmeA in enumerate(filmes):
+    print(f"Processando... {i}/{t}", end = '\r')
+    for j in range(i):
+        temp.append(None)
+    for filmeB in filmes[i+1:]:
+        temp.append(round(comp(filmeA, filmeB, tags, pesos), 2))
+        #temp.append(f'({filmeA[5]}, {filmeB[5]})') #debugOnly
+    matriz.append(temp)
     temp = []
 
-    t = len(filmes)
-
-    for i, filmeA in enumerate(filmes):
-        print(f"Processando... {i}/{t}", end = '\r')
-        for filmeB in filmes[i+1:]:
-            temp.append(round(comp(filmeA, filmeB, tags, pesos), 2))
-        matriz.append(temp)
-        temp = []
-    return matriz
-
-"""
-if __name__ == '__main__':
-    df = pd.read_csv('../../../data/archive/10test.csv')
-    filmes = df.values.tolist()
-        
-    tags = ['budget', 'genres', 'id', 'keywords', 'original_language', 'original_title', 'overview',
-            'popularity', 'production_companies', 'production_countries', 'release_date', 'revenue',
-            'runtime', 'title', 'vote_average', 'vote_count']
-
-    pesos = {'budget': 3, 'genres': 8, 'id': 0, 'keywords': 2, 'original_language': 1, 'original_title': 0, 'overview': 0,
-            'popularity': 3, 'production_companies': 1, 'production_countries': 1, 'release_date': 2, 'revenue': 4,
-            'runtime': 3, 'title': 0, 'vote_average': 7, 'vote_count': 6}
-
-    print(generate_matrix(filmes, tags, pesos))
-"""
+temp = df['original_title']
+newDf = pd.DataFrame(data= matriz[:-1], columns=[i for i in temp[1:]], index=[i for i in temp[:-1]])
+newDf.to_csv('../data/archive/results/processed_10test.csv')
