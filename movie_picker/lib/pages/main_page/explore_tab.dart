0b837740401dcbfc4f85f2e366e042f85b5e856e
FernandaSouzaa <104859098+FernandaSouzaa@users.noreply.github.com>
2023-03-08 15:17:33 -0300

    Update inicial tela de explorar

--- movie_picker/lib/pages/main_page/explore_tab.dart
+++ movie_picker/lib/pages/main_page/explore_tab.dart
@@ -5,8 +5,91 @@ class ExploreTab extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text("Explore Tab"),
+    final PageController pageController1 = PageController();
+    final PageController pageController2 = PageController();
+
+    final List<Widget> cards1 = List.generate(10, (index) {
+      return Card(
+        child: SizedBox(
+          height: 115,
+          width: 82,
+          child: Center(
+            child: Text("Card ${index + 1}"),
+          ),
+        ),
+      );
+    });
+
+    final List<Widget> cards2 = List.generate(10, (index) {
+      return Card(
+        child: SizedBox(
+          height: 5,
+          width: 5,
+          child: Center(
+            child: Text("Lista 2, Card ${index + 1}"),
+          ),
+        ),
+      );
+    });
+
+    final List<Widget> cards3 = List.generate(52, (index) {
+      return Card(
+        child: SizedBox(
+          height: 115,
+          width: 82,
+          child: Center(
+            child: Text("Card ${index + 1}"),
+          ),
+        ),
+      );
+    });
+
+    return Scaffold(
+      body: ListView(
+        children: [
+          const Padding(
+            padding: EdgeInsets.all(8.0),
+            child: Text(
+              "Principais escolhas",
+              style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+            ),
+          ),
+          SizedBox(
+            height: 150,
+            child: PageView(
+              controller: pageController1,
+              children: cards1,
+            ),
+          ),
+          const Padding(
+            padding: EdgeInsets.all(8.0),
+            child: Text(
+              "Assistir mais tarde",
+              style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+            ),
+          ),
+          SizedBox(
+            height: 150,
+            child: PageView(
+              controller: pageController2,
+              children: cards2,
+            ),
+          ),
+          const Padding(
+            padding: EdgeInsets.all(8.0),
+            child: Text(
+              "Outros Filmes",
+              style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+            ),
+          ),
+          GridView.count(
+            crossAxisCount: 3,
+            shrinkWrap: true,
+            physics: const NeverScrollableScrollPhysics(),
+            children: cards3,
+          ),
+        ],
+      ),
     );
   }
 }
