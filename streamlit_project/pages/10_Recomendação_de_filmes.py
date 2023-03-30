c3928e916b50233530425734d64b1b2232ab086d
Guilherme Henrique <guihenrique.dev@gmail.com>
2023-03-30 19:16:51 -0300

    Criando grafo/ Inicio da página de recomendação

--- streamlit_project/pages/10_Recomendação_de_filmes.py
+++ streamlit_project/pages/10_Recomendação_de_filmes.py
@@ -2,24 +2,54 @@ import itertools
 import pandas as pd
 from sklearn.cluster import SpectralClustering
 import streamlit as st
-import streamlit.components.v1 as components
-import matplotlib.pyplot as plt
-import mpld3
 import networkx as nx
-from networkx.algorithms import community
+from networkx import Graph
 from ast import literal_eval
 from node2vec import Node2Vec as n2v
-from sklearn.decomposition import PCA
-from sklearn.metrics import pairwise_distances
-import numpy as np
-import plotly.express as ply
-import plotly.graph_objects as go
 
-from pathlib import Path
-import sys
-sys.path.append(str(Path(__file__).resolve().parent.parent))
+def get_graph() -> Graph:
+    G = nx.read_gpickle(f'grafo_generos.pickle')
+    return G
 
-from matrix_generation.matriz_de_similaridade import generate_matrix
+df = pd.read_csv(f'streamlit_project/data/archive/tmdb_3000_ranges.csv', converters={'genres': literal_eval, 'keywords': literal_eval, 'production_companies': literal_eval, 'production_countries': literal_eval, 'cast': literal_eval, 'director': literal_eval})
+G = get_graph()
+nodes_and_index = G.nodes.data('name')
+
+config_manu = st.checkbox('Configurar parâmetros manualmente?', False)
+
+cut_value = 0.0
+WINDOW = 1
+MIN_COUNT = 1
+BATCH_WORDS = 4
+DIMENSIONS = 56
+
+if config_manu:
+    c3, c4, c5, c6, c7 = st.columns(5)
+    with c3: cut_value = st.slider("Valor mínimo para conexão: ", 0.01, 1.0, step=0.01)
+    with c4: WINDOW = st.slider("Janela (distância máxima entre entre nós): ", 1, 25, step=1)
+    with c5: MIN_COUNT = st.slider("Valor mínimo (ignora nós com frequência total menor que esta): ", 1, 25, step=1)
+    with c6: BATCH_WORDS = st.slider("Tamanho do lote para processamento paralelo: ", 1, 25, step=1)
+    with c7: DIMENSIONS = st.slider("Dimensões (número de dimensões usadas para representar cada nó): ", 2, 256, step=8)
+
+@st.cache_resource
+def execute_n2v(G):
+    g_emb = n2v(G, dimensions=DIMENSIONS, seed=128, weight_key='weight', walk_length=80, num_walks=10, workers=3, temp_folder='temp_folder/')
+    mdl = g_emb.fit(
+        vector_size=DIMENSIONS,
+        window=WINDOW,
+        min_count=MIN_COUNT,
+        batch_words=BATCH_WORDS,
+    )
+    return mdl
+
+mdl = execute_n2v(G)
+
+emb_df = (
+    pd.DataFrame(
+        [mdl.wv.get_vector(str(n)) for n in G.nodes()],
+        index = nodes_and_index
+    )
+)
 
 st.markdown(
     """
@@ -31,28 +61,25 @@ st.markdown(
     """
 )
 
-rec_forms = ['node2vec', 'Vizinhos mais próximos']
+base_film = st.selectbox('Selecione o filme base: ', df['title'])
+
+rec_forms = ['Similaridade de Cosseno', 'Clusters', 'Vizinho mais próximo']
 rec_form = ''
 
 st.subheader('Forma de recomendação')
 rec_form = st.selectbox('Selecione a forma de recomendação: ', rec_forms)
 
-cut_value = 0.0
-WINDOW = 1
-MIN_COUNT = 1
-BATCH_WORDS = 4
-DIMENSIONS = 16
+if rec_form == 'Similaridade de Cosseno':
 
-st.subheader('Parâmetros extras: ')
-if rec_form == 'node2vec':
-    c3, c4, c5, c6, c7 = st.columns(5)
-    with c3: cut_value = st.slider("Valor mínimo para conexão: ", 0.01, 1.0, step=0.01)
-    with c4: WINDOW = st.slider("Janela (distância máxima entre entre nós): ", 1, 25, step=1)
-    with c5: MIN_COUNT = st.slider("Valor mínimo (ignora nós com frequência total menor que esta): ", 1, 25, step=1)
-    with c6: BATCH_WORDS = st.slider("Tamanho do lote para processamento paralelo: ", 1, 25, step=1)
-    with c7: DIMENSIONS = st.slider("Dimensões (número de dimensões usadas para representar cada nó): ", 2, 256, step=8)
+    base_film_index = ""
+    for node in nodes_and_index:
+        if base_film == node[1]:
+            base_film_index = node[0]
+    similars = mdl.wv.most_similar(str(base_film_index), topn=10)
+    similars_list = [int(item[0]) for item in similars]
+    df_closest = emb_df.loc[similars_list]
 
-    st.write('TODO: aplicar node2vec no grafo inteiro, selecionar o filme a ser recomendado e efetivamente recomendar o filme')
+    st.write(df_closest)
 
 if rec_form == 'Vizinhos mais próximos':
     pass
