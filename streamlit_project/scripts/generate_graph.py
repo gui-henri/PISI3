7c5726de7c82e1bfa6e11186067206103f719168
Guilherme Henrique <guihenrique.dev@gmail.com>
2023-04-09 17:39:26 -0300

    correção nas páginas iniciais

--- streamlit_project/scripts/generate_graph.py
+++ streamlit_project/scripts/generate_graph.py
@@ -14,7 +14,7 @@ from matrix_generation.matriz_de_similaridade import generate_matrix
 # ler o arquivo
 def generate(cut_value, pesos):
 
-    df = pd.read_csv('streamlit_project/data/archive/tmdb_3000_binario.csv', converters={'genres': literal_eval, 'keywords': literal_eval, 'production_companies': literal_eval, 'production_countries': literal_eval, 'cast': literal_eval, 'director': literal_eval})
+    df = pd.read_csv('streamlit_project/data/archive/5k_movies_bin.csv', converters={'genres': literal_eval, 'keywords': literal_eval, 'production_companies': literal_eval, 'production_countries': literal_eval, 'cast': literal_eval, 'director': literal_eval})
 
     maxPesos = sum(pesos.values())
     movies = df.values.tolist()
@@ -38,13 +38,13 @@ if __name__ == "__main__":
                 'vote_average': 0, 'vote_count': 0, 'runtime': 1, 'release_date': 1, 'original_language': 0,
                 'production_countries': 0, 'production_companies': 0, 'director': 1, 'cast': 1}
      cut_value = 0.4
-     G = generate(cut_value, pesos)
+     G = nx.gpickle.read_gpickle('grafo_old.pickle')
      WINDOW = 1
      MIN_COUNT = 1
      BATCH_WORDS = 1
-     DIMENSIONS = 4
+     DIMENSIONS = 100
      print('Processando node2vec...')
-     g_emb = n2v(G, dimensions=DIMENSIONS, seed=128, weight_key='weight', walk_length=80, num_walks=10, workers=3, temp_folder='temp_folder/')
+     g_emb = n2v(G, p=1, q=0.5, dimensions=DIMENSIONS, seed=128, weight_key='weight', walk_length=5, num_walks=100, workers=3, temp_folder='temp_folder/')
      print('Treinando modelo node2vec...')
      mdl = g_emb.fit(
         vector_size=DIMENSIONS,
@@ -53,6 +53,6 @@ if __name__ == "__main__":
         batch_words=BATCH_WORDS,
     )
      print('Salvando modelo node2vec...')
-     mdl.save('n2v_model.w2v')
+     mdl.save('n2v_model_2.w2v')
      print('Execução concluída!')
 
