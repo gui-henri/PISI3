370fa1713f7229f00ac3c5eadad63520bc5c7a09
RennanJSM <87147821+RennanJSM@users.noreply.github.com>
2023-04-27 14:51:56 -0300

    Adição comparar

--- streamlit_project/scripts/Processamento_Analise_de_incidencia.py
+++ streamlit_project/scripts/Processamento_Analise_de_incidencia.py
@@ -2,6 +2,7 @@ import pandas as pd
 from ast import literal_eval
 from collections import Counter
 import json
+import comparar
 
 df = pd.read_csv('streamlit_project/data/archive/5k_movies_bin.csv', converters={'genres': literal_eval, 'keywords': literal_eval, 'production_companies': literal_eval, 'production_countries': literal_eval, 'cast': literal_eval, 'director': literal_eval})
 movies = df.values.tolist()
